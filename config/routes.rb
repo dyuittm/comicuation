d9f18ea20d29b1a563e41948da5fce91c27fe3fa
dyuittm <yuuuth0326@gmail.com>
2022-08-03 14:03:35 +0000

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, skip: [:registrations], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
