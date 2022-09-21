501bc5232b82d989c94a0eb5221af73f45028534
dyuittm <yuuuth0326@gmail.com>
2022-09-21 07:33:12 +0000

    group admin

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,17 +1,28 @@
 class Admin::GroupsController < ApplicationController
+  before_action :ensure_correct_admin, only: [:show, :edit, :update, :destroy]
 
   def index
     @groups = Group.all.order(updated_at: :desc)
   end
 
   def show
-    @group = Group.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to admin_groups_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    flash[:notice] = "削除しました"
+    redirect_to admin_groups_path
   end
 
   private
@@ -19,7 +30,7 @@ class Admin::GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, :introduction, :group_image, :user_id [])
   end
-  
+
   def ensure_correct_admin
     @group = Group.find(params[:id])
   end
